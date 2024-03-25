e1c8f52da88e260faed6f9ac5660e32b8b580f86
ALLAN <allankipr6@gmail.com>
2024-03-25 12:14:18 +0300

    mew

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+const java= [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    
+  ]
+
+    
+function superbowlWin(java){
+    const result=java.find(java=>java.result==="W")
+    return result ? result.year : undefined;
+}
